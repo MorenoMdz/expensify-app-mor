2879739e4e90f36b73c0039205cdc7cf310a016d
Moreno <morenomdz@gmail.com>
2019-01-05 23:33:36 -0200

    set initial data

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -5,12 +5,27 @@ import {
   addExpense,
   editExpense,
   removeExpense,
+  setExpenses,
+  startSetExpenses,
 } from '../../actions/expenses';
 import expenses from '../fixtures/expenses';
 import database from '../../firebase/firebase';
 
 // create the configuration to generate the same mock store multiple times
 const createMockStore = configureMockStore([thunk]);
+
+// runs before each test, async, it waits for the done() call
+beforeEach(done => {
+  const expensesData = {};
+  // loop over the expenses and add the data properly parsed
+  expenses.forEach(({ id, description, note, amount, createdAt }) => {
+    expensesData[id] = { description, note, amount, createdAt };
+  });
+  database
+    .ref('expenses')
+    .set(expensesData)
+    .then(() => done());
+});
 
 test('should setup remove expense action object', () => {
   const action = removeExpense({ id: '123abc' });
@@ -95,16 +110,24 @@ test('should add expense to database and store', done => {
     });
 });
 
-// test('should setup add expense action object with default values', () => {
-//   const action = addExpense();
-//   expect(action).toEqual({
-//     type: 'ADD_EXPENSE',
-//     expense: {
-//       id: expect.any(String),
-//       description: '',
-//       note: '',
-//       amount: 0,
-//       createdAt: 0
-//     }
-//   });
-// });
+test('should setup set expense action object with data', () => {
+  const action = setExpenses(expenses);
+  expect(action).toEqual({
+    type: 'SET_EXPENSES',
+    expenses,
+  });
+});
+
+test('should fetch the expenses from firebase', done => {
+  // create a mock store data
+  const store = createMockStore({});
+  // dispatch the action to set the initial data
+  store.dispatch(startSetExpenses()).then(() => {
+    const actions = store.getActions();
+    expect(actions[0]).toEqual({
+      type: 'SET_EXPENSES',
+      expenses,
+    });
+    done();
+  });
+});
